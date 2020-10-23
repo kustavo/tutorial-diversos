78863869e891e5330b3d8274cfc53ed3bb24b1dc
Gustavo Araujo <kustavo@gmail.com>
2020-10-22 22:10:53 -0300

    update

--- public/java/linguagem/_modificadores/exemplo_acesso_classes/pacote2/Classe3.java
+++
@@ -1,7 +0,0 @@
-package _modificadores.exemplo_acesso_classes.pacote2;
-
-// import pacote1.Classe1; // ERRO!
-
-public class Classe3 {
-
-}
--- public/java/linguagem/_modificadores/exemplo_native/TesteNative.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <jni.h>
-#include "TesteNative.h"
-
-JNIEXPORT void JNICALL
-Java_TesteNative_imprimir(JNIEnv *env, jobject obj) {
-  printf("Olá! Invocando um método nativo!");
-  return;
-}
